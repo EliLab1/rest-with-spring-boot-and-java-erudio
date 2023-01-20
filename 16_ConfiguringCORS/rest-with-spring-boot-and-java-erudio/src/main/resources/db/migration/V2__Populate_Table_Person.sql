3deba307bdd74fcffb160d97acf893acabe872a2
unknown <elinhozs.br@gmail.com>
2023-01-20 19:10:55 -0300

    dockerrizing our Application

---
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Elisson', 'Male', 'Fernandes'),
+	(4, 'Porto Alegre', 'Eduardo', 'Male', 'Geske'),
+	(5, 'Caieiras', 'Raphael', 'Male', 'Poliesi'),
+	(7, 'RJ', 'Coxinha', 'Female', 'de Frango'),
+	(8, 'RJ', 'Coxinha', 'Female', 'de Carne');
